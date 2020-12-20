105e075b862f1c5f6298e7f1e2cf54bda1b953ae
AmyRaff <51639064+AmyRaff@users.noreply.github.com>
2020-12-20 16:00:36 +0000

    Add files via upload

---
+++ Drone/aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/Sensor.java
@@ -0,0 +1,46 @@
+package uk.ac.ed.inf.aqmaps;
+
+public class Sensor {
+
+	private Coordinate coordinates;
+	private String words;
+	private boolean visited = false;
+	
+	private static class Coordinate {
+		private double lng;
+		private double lat;
+	}
+
+	public double getLatitude() {
+		return coordinates.lat;
+	}
+	
+	public double getLongitude() {
+		return coordinates.lng;
+	}
+
+	public String getWords() {
+		return words;
+	}
+	
+	// get colour of marker for the sensor
+	public String getColour() {
+		var rgb = MapUtils.getSensorColour(this);
+		return rgb;
+	}
+	
+	// get symbol of marker for the sensor
+	public String getSymbol() {
+		var symbol = MapUtils.getSensorSymbol(this);
+		return symbol;
+	}
+	
+	public void setVisited() {
+		visited = true;
+	}
+	
+	public boolean isVisited() {
+		return visited;
+	}
+	
+}
